65630d7d0c7c815cdbeef50373e272b309f505e6
Gomez Santiago <santygomez355@gmail.com>
2025-08-21 21:18:02 -0300

    nuevas cosas
    patos

--- EjeOrnitorrinco/src/ejeornitorrinco/Pato.java
+++ EjeOrnitorrinco/src/ejeornitorrinco/Pato.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package ejeornitorrinco;
 
 /**
  *
  * @author Usuario
  */
 public class Pato {
-    
-    //hola pato
+    // hola pato
     
 }
