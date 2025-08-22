0b2a52b7cd56561b97b260b7d92873668719360a
Gomez Santiago <santygomez355@gmail.com>
2025-08-22 15:31:08 -0300

    Agregados a clase Castor
    completo la clase castor con Random, agrego get y set para poder modificar cola y velocidad que se muetra con un sout

--- EjeOrnitorrinco/src/ejeornitorrinco/Castor.java
+++ EjeOrnitorrinco/src/ejeornitorrinco/Castor.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejeornitorrinco;
 
-/**
- *
- * @author arceb
- */
+ import java.util.Random;
+
 public class Castor {
     protected double longitudCola;
     protected double velocidad;
@@ -16,12 +11,38 @@ public class Castor {
         this.longitudCola = longitudCola;
         this.velocidad = velocidad;
     }
-    
+// agregamos geter y seter para poder modificar cola y velocidad
+    public double getLongitudCola() {
+        return longitudCola;
+    }
+
+    public void setLongitudCola(double longitudCola) {
+        this.longitudCola = longitudCola;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+    // le agrego el Random como pide en la consigna que tiene que ser un nro de 1 a 6 (como las cuerdas).
     public void tocarGuitarra(){
-    
+     Random random = new Random();
+        System.out.println("El castor toca la guitarra");
+        for (int i = 0; i < 3; i++){
+            int cuerda = random.nextInt(6) + 1;
+            System.out.println("Toca cuerda" + cuerda);    
+        }
     }
-    
+    // le agrego el sout para mostrar a la velocidad que tiene que ir el castor "8km/h"
     public void nadar(){
-        
+        System.out.println("El castor nada a " + velocidad + "km/h");
     }
+   
+   
+    public String tostring(){
+       return "Castor Cola: " + longitudCola + "cm, Velocidad: " + velocidad + " km/h";
+   }
 }
