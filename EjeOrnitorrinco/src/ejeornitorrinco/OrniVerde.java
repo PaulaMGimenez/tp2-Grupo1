72efa2288b94405b6dd7c72ab955deb8a98f781f
MLMartinLeiza <martin.leiza@gmail.com>
2025-08-24 19:19:25 -0300

    Commit de prueba cambios minimos

--- EjeOrnitorrinco/src/ejeornitorrinco/OrniVerde.java
+++ EjeOrnitorrinco/src/ejeornitorrinco/OrniVerde.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejeornitorrinco;
 
-/**
- *
- * @author arceb
- */
 public class OrniVerde extends Castor implements MamaPato {
     private String nombre;
     public OrniVerde(double longitudCola, double velocidad, String nombre) {
--- EjeOrnitorrinco/src/ejeornitorrinco/OrnisAzul.java
+++ EjeOrnitorrinco/src/ejeornitorrinco/OrnisAzul.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejeornitorrinco;
 
-/**
- *
- * @author arceb
- */
 public class OrnisAzul extends Castor {
     private double propulsion;
     private String nombre;
