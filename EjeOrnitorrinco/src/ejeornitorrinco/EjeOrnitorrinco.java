ef2b4ef74cc814ed4e0093f34361f486f53b9a0f
Eduardo Gimenez <vinchucon@gmail.com>
2025-08-25 20:53:05 -0300

    Update EjeOrnitorrinco.java
    agregue ArrayList - comparator - Iterator

--- EjeOrnitorrinco/src/ejeornitorrinco/EjeOrnitorrinco.java
+++ EjeOrnitorrinco/src/ejeornitorrinco/EjeOrnitorrinco.java
@@ -1,5 +1,7 @@
 package ejeornitorrinco;
 
+import java.util.*;
+
 public class EjeOrnitorrinco {
 
     public static void main(String[] args) {
@@ -63,7 +65,29 @@ public class EjeOrnitorrinco {
         
         
         
+                // Lista de mejores nadadores
+        ArrayList<OrnisAzul> mejoresNad = new ArrayList<>();
         
+        
+        for (int i = 0; i < hermanos.length; i++) {
+            Castor h = hermanos[i];
+            if (h instanceof OrnisAzul) {
+                mejoresNad.add((OrnisAzul) h);
+            }
+        }
+        
+        
+        
+        // Ordenar lista por propulsion
+        mejoresNad.sort(Comparator.comparingDouble(OrnisAzul::getPropulsion));
+
+        // Mostrar lista ordenada
+        System.out.println("---Lista propulsión ascendente---");
+        Iterator<OrnisAzul> it = mejoresNad.iterator();
+        while (it.hasNext()) {
+            OrnisAzul o = it.next();
+            System.out.println(o.getNombre() + " con propulsión: " + o.getPropulsion());
+        }
     
         
         
